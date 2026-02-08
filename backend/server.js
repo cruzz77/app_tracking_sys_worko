cc274d183630800d2be659745256614cc26cde09
Aditya Chopra <nytc120605@gmail.com>
2026-02-08 13:42:43 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options('*', cors());
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
